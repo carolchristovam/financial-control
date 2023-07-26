9bb796033c75730ef10ad8cae3ca280f13795c3f
Carol <carolchristovamc@gmail.com>
2023-07-26 12:32:20 -0300

    new control screen

---
+++ app/src/main/java/com/example/financial_control/Controls.kt
@@ -0,0 +1,11 @@
+package com.example.financial_control
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Controls : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controls)
+    }
+}
