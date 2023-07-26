3d48f8c4d5bf0029c2a7419026e4ed599d779990
Carol <carolchristovamc@gmail.com>
2023-07-26 15:01:31 -0300

    feat: connecting to activity_controls

--- app/src/main/java/com/example/financial_control/Controls.kt
+++ app/src/main/java/com/example/financial_control/Controls.kt
@@ -2,10 +2,17 @@ package com.example.financial_control
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.financial_control.databinding.ActivityControlsBinding
 
 class Controls : AppCompatActivity() {
+
+    private lateinit var binding: ActivityControlsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_controls)
+
+        binding = ActivityControlsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
